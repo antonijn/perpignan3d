a5cf6b72f273f07ac42305ad603b3f5a007bf247
Antonie Blom <ablom@posteo.net>
2021-11-30 21:43:34 +0100

    Add basic tile adding functionality

--- perp3d.py
+++ perp3d.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-import sys
+import math
 import os.path
 import perpignan as p
+import sys
+import threading
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import AmbientLight, DirectionalLight, LightAttrib
-from panda3d.core import LPoint3, LVector3, BitMask32
+from panda3d.core import Point3, Vec2, Vec3, Plane
 from panda3d.core import OrthographicLens
 from panda3d.core import NodePath
 from panda3d.core import loadPrcFileData
 from direct.showbase import DirectObject
+from direct.task import Task
 from enum import Enum
 
 loadPrcFileData('', 'framebuffer-srgb true')
@@ -178,86 +181,222 @@ def tile_to_node(tile, scene):
         middle_inst.instanceTo(middle)
         middle.setH(middle, middle_rot)
 
+    node.setScale(0.5, 0.5, 0.5)
+
     return node
 
+class MyPlayer(p.Player):
+    def __init__(self, action_avail_cond):
+        super().__init__('3d!')
+        self.action_avail_cond = action_avail_cond
+        self.action = None
+
+    def poll_action(self):
+        self.action_avail_cond.acquire()
+        self.action_avail_cond.wait()
+        return self.action
+
+    def do_action(self, action):
+        self.action_avail_cond.acquire()
+        try:
+            self.action = action
+            self.action_avail_cond.notify()
+        finally:
+            self.action_avail_cond.release()
+
+    def inform(self, msg):
+        pass
+
+class GhostTile(DirectObject.DirectObject):
+    def __init__(self, app, player, cursor=(43, 42)):
+        super().__init__()
+
+        self.tile = app.perp.deck[-1]
+        self.node = tile_to_node(self.tile, app.scene)
+        x, y = cursor
+        self.node.setPos(x, y, 0)
+        self.node.reparentTo(app.render)
+
+        self.app = app
+        self.player = player
+
+        self.hpr_lerp = None
+        self.rotations = 0
+        self.cursor = cursor
+
+        self.update_task = taskMgr.add(self.update, 'update')
+
+        # make these object-bound, so we create less garbage in the
+        # update function
+        self.mousePos3D = Point3()
+        self.nearPoint = Point3()
+        self.farPoint = Point3()
+        self.groundPlane = Plane(Vec3(0, 0, 1), Vec3(0, 0, 0))
+        self.lastMousePos = Vec2(0, 0)
+
+        self.accept('wheel_up', self.rotCW)
+        self.accept('wheel_down', self.rotCCW)
+        self.accept('mouse1', self.place)
+
+    def rotCW(self):
+        self.rotations = (self.rotations + 1) % 4
+        initial_h = self.node.getH()
+
+        if self.hpr_lerp is not None and self.hpr_lerp.isPlaying():
+            self.hpr_lerp.finish()
+
+        h = self.node.getH() - 90
+        self.hpr_lerp = self.node.hprInterval(0.15, Vec3(h, 0, 0), Vec3(initial_h, 0, 0))
+        self.hpr_lerp.start()
+
+    def rotCCW(self):
+        self.rotations = (self.rotations + 3) % 4
+        initial_h = self.node.getH()
+
+        if self.hpr_lerp is not None and self.hpr_lerp.isPlaying():
+            self.hpr_lerp.finish()
+
+        h = self.node.getH() + 90
+        self.hpr_lerp = self.node.hprInterval(0.15, Vec3(h, 0, 0), Vec3(initial_h, 0, 0))
+        self.hpr_lerp.start()
+
+    def commitTransforms(self):
+        x, y = self.cursor
+        self.app.perp.cursor = self.cursor
+        for _ in range(self.rotations):
+            self.app.perp.deck[-1].rotate_cw()
+        self.rotations = 0
+
+    def canPlace(self):
+        return self.app.perp.can_place()
+
+    def place(self):
+        self.commitTransforms()
+
+        if not self.canPlace():
+            return
+
+        self.app.perp.place()
+        messenger.send('tile_placed')
+
+    def update(self, task):
+        mouseWatcher = base.mouseWatcherNode
+        if mouseWatcher.hasMouse():
+            mousePos = mouseWatcher.getMouse()
+            self.lastMousePos = mousePos
+        else:
+            mousePos = self.lastMousePos
+
+        base.camLens.extrude(mousePos, self.nearPoint, self.farPoint)
+        self.groundPlane.intersectsLine(
+            self.mousePos3D,
+            base.render.getRelativePoint(base.cam, self.nearPoint),
+            base.render.getRelativePoint(base.cam, self.farPoint)
+        )
+
+        cur_x, cur_y, _ = self.mousePos3D
+        def dist(pos):
+            x, y = pos
+            x -= cur_x
+            y -= cur_y
+            return x*x + y*y
+
+        nearest_spot = min(self.app.perp.available, key=dist)
+        self.cursor = nearest_spot
+        self.node.setPos(nearest_spot[0], nearest_spot[1], 0)
+
+        return Task.cont
+
+    def remove(self):
+        #self.node.removeNode()
+        self.ignoreAll()
+        taskMgr.remove(self.update_task)
+
 class MyApp(ShowBase):
-    def __init__(self):
+    def __init__(self, perp):
         ShowBase.__init__(self)
+        self.perp = perp
 
         self.setBackgroundColor(0.392, 0.584, 0.929)
         tiles_path = os.path.join(os.path.dirname(__file__), 'tiles.gltf')
         self.scene = self.loader.loadModel(tiles_path)
 
-        self.perp = p.Perpignan()
-        self.tnode = None
-
-        self.tile = -1
-        self.nextTile()
+        start_tile = tile_to_node(self.perp.grid[42][42], self.scene)
+        start_tile.setPos(42, 42, 0)
+        start_tile.reparentTo(self.render)
 
         self.setupLights()
 
-        lens = OrthographicLens()
-        #lens.setFilmSize(2, 2)
-        self.cam.node().setLens(lens)
-        self.cam.setPos(8, -4, 12)
-        self.cam.lookAt(LVector3(0, 0, 0))
+        self.disableMouse()
+        self.camLens = OrthographicLens()
+        self.cam.node().setLens(self.camLens)
+        self.focus = Vec3(42, 42, 0)
+        self.tiles_placed = 1
+        # this initial setting is meaningless; see next_player()
+        self.focus_radius = 1
 
         self.accept(self.win.getWindowEvent(), self.onWindowEvent)
         self.accept('escape', sys.exit)
-        self.accept('k', self.nextTile)
-        self.accept('j', self.prevTile)
-        self.accept('wheel_up', self.rotCW)
-        self.accept('wheel_down', self.rotCCW)
+        self.accept('tile_placed', self.next_player)
+        self.ghost = None
+        self.next_player()
+
+    def next_player(self):
+        self.perp.next_player()
+        if self.ghost is not None:
+            self.ghost.remove()
+            x, y = self.ghost.cursor
+            self.focus = (self.focus * self.tiles_placed + Vec3(x, y, 0)) / (self.tiles_placed + 1)
+            self.tiles_placed += 1
+
+        far_tile_dist_sqr = max(
+            (Vec3(t.x, t.y, 0) - self.focus).length_squared()
+            for col in self.perp.grid
+            for t in col
+            if t is not None
+        )
+        self.focus_radius = math.sqrt(far_tile_dist_sqr)
+
+        self.ghost = GhostTile(self, self.perp.active_player)
+
+        self.reset_camera()
 
     def onWindowEvent(self, window):
         if window.isClosed():
             sys.exit()
 
-        width = window.getProperties().getXSize()
-        height = window.getProperties().getYSize()
+        self.reset_camera()
+
+    def reset_camera(self):
+        width = self.win.getProperties().getXSize()
+        height = self.win.getProperties().getYSize()
         aspect = width / height
-        fw, fh = 6, 6
+        fw = (self.focus_radius + 3) * 1.4
+        fh = fw
         if width > height:
             fw *= aspect
         else:
             fh /= aspect
-        lens = self.cam.node().getLens()
-        lens.setFilmSize(fw, fh)
-
-    def nextTile(self):
-        self.tile = (self.tile + 1) % len(self.perp.deck)
-        self.loadTile()
-
-    def prevTile(self):
-        self.tile = (self.tile + len(self.perp.deck) - 1) % len(self.perp.deck)
-        self.loadTile()
-
-    def loadTile(self):
-        if self.tnode is not None:
-            self.tnode.removeNode()
-
-        tile = self.perp.deck[self.tile]
-
-        self.tnode = tile_to_node(tile, self.scene)
-        #self.tnode.setPos(-8, 4, -12)
-        self.tnode.reparentTo(self.render)
-
-    def rotCW(self):
-        h = self.tnode.getH(self.tnode)
-        self.tnode.setH(self.tnode, h - 90)
-
-    def rotCCW(self):
-        h = self.tnode.getH(self.tnode)
-        self.tnode.setH(self.tnode, h + 90)
+        self.camLens.setFilmSize(fw, fh)
+        self.cam.setPos(self.focus + Vec3(8, -4, 12))
+        self.cam.lookAt(self.focus)
 
     def setupLights(self):  # This function sets up some default lighting
         ambientLight = AmbientLight("ambientLight")
         ambientLight.setColor((.7, .7, .7, 1))
         directionalLight = DirectionalLight("directionalLight")
-        directionalLight.setDirection(LVector3(0, 45, -45))
+        directionalLight.setDirection(Vec3(0, 45, -45))
         directionalLight.setColor((.3, .3, .3, 1))
         render.setLight(render.attachNewNode(directionalLight))
         render.setLight(render.attachNewNode(ambientLight))
 
-app = MyApp()
+perp = p.Perpignan()
+action_avail_cond = threading.Condition()
+perp.players = [MyPlayer(action_avail_cond)]
+
+app = MyApp(perp)
+
+#perp_thread = threading.Thread(target=perp.run)
+#perp_thread.start()
+
 app.run()
